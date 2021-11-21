91e5b640e46140d0b92e304aa4ea8863bf487107
alliedrake <71614898+alliedrake@users.noreply.github.com>
2021-11-21 18:48:46 -0500

    Update footer.php
    Fixed path

--- _global/footer.php
+++ _global/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
-    <a href="home.php"><img src="../dist/images/foodie.png" alt="banner"></a>
+    <a href="home.php"><img src="/../dist/images/foodie.png" alt="banner"></a>
     <!-- <a href="<?php //echo siteUrl('/auth/logout.php'); ?>">Log</a>
 </div> -->
 
