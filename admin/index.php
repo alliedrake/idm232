a7cf3d66ecc1aa45f71bd268f8440732d90917f9
alliedrake <71614898+alliedrake@users.noreply.github.com>
2021-11-21 18:49:07 -0500

    Update index.php
    Added content to blank dashboard

--- admin/index.php
+++ admin/index.php
@@ -8,6 +8,10 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/_global/header.php';
     <h1>Dashboard</h1>
 </div>
 
+<a class="createbutton" href="users/">Staff</a>
+<a class="createbutton" href="recipes/">Recipes</a>
+
+
 
 
 <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/_global/header.php';?>
