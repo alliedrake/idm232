bcd5f586a4d417d01ae8ed590a5a9633cf2b087e
alliedrake <71614898+alliedrake@users.noreply.github.com>
2021-11-22 22:00:20 -0500

    Create list-recipes.php
    Table for listing recipes added

---
+++ _components/list-recipes.php
@@ -0,0 +1,29 @@
+<div class="table_container">
+
+
+<table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+
+    
+  
+    <tbody>
+      
+        <?php
+          while ($row = mysqli_fetch_assoc($db_results)) {
+              echo '<tr>';
+              echo '<td>' . $row['id'] . '</td>';
+              echo '<td>' . $row['title'] . '</td>';
+              echo '<td><a href="view_recipe.php?id=' . $row['id'] . '">' . $row['description']. '</a></td>';
+              echo '</td>';
+          }
+        ?> 
+    </tbody>
+        
+  </table>
+        </div>
